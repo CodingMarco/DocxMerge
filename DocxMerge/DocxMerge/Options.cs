44ef305fb9b8463f5bcb6d9fb4e2401dae62b30a
Marco von Rosenberg <marco.von-rosenberg@keysight.com>
2024-11-08 12:10:58 +0100

    code improvements made possible by .NET 8

--- DocxMerge/DocxMerge/Options.cs
+++ DocxMerge/DocxMerge/Options.cs
@@ -1,28 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CommandLine;
-using CommandLine.Text;
+﻿using CommandLine;
 
-namespace DocxMerge
+namespace DocxMerge;
+
+class Options
 {
-    class Options
-    {
-        [Option('i', "input", Required = true, HelpText = "A list of docx files to merge in order")]
-        public IEnumerable<string> InputFiles { get; set; }
+    [Option('i', "input", Required = true, HelpText = "A list of docx files to merge in order")]
+    public IEnumerable<string> InputFiles { get; set; }
 
-        [Option('o', "output", Required = false, HelpText = "Output file [Default: output.docx]")]
-        public string Output { get; set; }
+    [Option('o', "output", Required = false, HelpText = "Output file [Default: output.docx]")]
+    public string Output { get; set; }
 
-        [Option('f', "Force", Required = false, HelpText = "Replace output if already exists")]
-        public bool Force { get; set; }
+    [Option('f', "Force", Required = false, HelpText = "Replace output if already exists")]
+    public bool Force { get; set; }
 
-        [Option('r', "Repair-Spacing", Required = false, HelpText = "Replace single spacing after sentences removed from pandoc")]
-        public bool RepairSpacing { get; set; }
+    [Option('r', "Repair-Spacing", Required = false, HelpText = "Replace single spacing after sentences removed from pandoc")]
+    public bool RepairSpacing { get; set; }
 
-        [Option('v', "Verbose", Required = false, HelpText = "Show more information when executing")]
-        public bool Verbose { get; set; }
-    }
+    [Option('v', "Verbose", Required = false, HelpText = "Show more information when executing")]
+    public bool Verbose { get; set; }
 }
--- DocxMerge/DocxMerge/Program.cs
+++ DocxMerge/DocxMerge/Program.cs
@@ -1,153 +1,143 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿using CommandLine;
 using DocumentFormat.OpenXml.Packaging;
-using System.IO;
+using System.Text.RegularExpressions;
 using System.Xml;
 using System.Xml.Linq;
-using CommandLine;
 
-namespace DocxMerge
+namespace DocxMerge;
+
+static partial class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        IEnumerable<string> inputFiles = null;
+        string output = null;
+        bool verbose = false;
+        bool force = false;
+        bool repairSpacing = false;
+
+        var results = Parser.Default.ParseArguments<Options>(args);
+        results.MapResult(options =>
         {
-            IEnumerable<string> inputFiles = null;
-            string output = null;
-            bool verbose = false;
-            bool force = false;
-            bool repairSpacing = false;
-
-            var results = CommandLine.Parser.Default.ParseArguments<Options>(args);
-            results.MapResult(options =>
-            {
-                inputFiles = options.InputFiles;
-                output = options.Output ?? "output.docx";
-                verbose = options.Verbose;
-                force = options.Force;
-                repairSpacing = options.RepairSpacing;
-                return 0;
-            }, errors =>
-            {
-                Environment.Exit(1);
-                return 1; // ._.
-            });
-
-
-            if (inputFiles.Count() < 2) ExitWithError("There must be at least two input files");
-            foreach (var file in inputFiles)
-            {
-                if (!File.Exists(file))
-                    ExitWithError("Unable to find {0}", file);
-            }
-
-            try {
-                if (verbose) Console.WriteLine("Creating initial document");
-                File.Copy(inputFiles.First(), output, force);
-
-                if (verbose) Console.WriteLine("Opening {0} for writing", output);
-                using (WordprocessingDocument doc = WordprocessingDocument.Open(output, true))
-                {
-                    int fileId = 0;
-                    foreach (var filepath in inputFiles.Skip(1))
-                    {
-                        if (verbose) Console.WriteLine("Adding {0} to {1}", filepath, output);
-                        fileId++;
-                        XNamespace w = "http://schemas.openxmlformats.org/wordprocessingml/2006/main";
-                        XNamespace r = "http://schemas.openxmlformats.org/officeDocument/2006/relationships";
-
-                        string altChuckId = string.Format("AltChuckId{0}", fileId);
-                        var mainPart = doc.MainDocumentPart;
-                        var chunk = mainPart.AddAlternativeFormatImportPart(
-                            "application/vnd.openxmlformats-officedocument.wordprocessingml.document.main+xml",
-                            altChuckId);
-
-                        if (repairSpacing)
-                            RepairSentenceSpacing(filepath, verbose);
-
-                        using (FileStream fileStream = File.Open(filepath, FileMode.Open))
-                            chunk.FeedData(fileStream);                        
-                        var altChunk = new XElement(w + "altChunk", new XAttribute(r + "id", altChuckId));
-                        var mainDocumentXDoc = GetXDocument(doc);
-                        
-                        mainDocumentXDoc.Root.Element(w + "body").Elements(w + "p").Last().AddAfterSelf(altChunk);                        
-                        SaveXDocument(doc, mainDocumentXDoc);                        
-                    }
-                }
-                
-                if (verbose) Console.WriteLine("Successfully merged all documents");
-            }
-            catch (Exception ex)
-            {
-                if (verbose) ExitWithError(ex.ToString());
-                else ExitWithError("DocxMerge failed to process the files: {0}", ex.Message);
-            }
-        }
-
-        private static void RepairSentenceSpacing(string filepath, bool verbose)
+            inputFiles = options.InputFiles;
+            output = options.Output ?? "output.docx";
+            verbose = options.Verbose;
+            force = options.Force;
+            repairSpacing = options.RepairSpacing;
+            return 0;
+        }, _ =>
         {
-            using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(filepath, true))
-            {
-                string docText = null;
-                using (StreamReader sr = new StreamReader(wordDoc.MainDocumentPart.GetStream()))
-                {
-                    docText = sr.ReadToEnd();
-                }
-
-                Regex regexText = new Regex(@"\. (?<a>[A-Z])");
-                var refIndex = docText.IndexOf("References");
-                if (refIndex > 0)
-                {
-                    var bodyText = docText.Substring(0, refIndex);
-                    var references = docText.Substring(bodyText.Length);
-                    docText = regexText.Replace(bodyText, ".  ${a}") + references;
-                }
-                else
-                {
-                    docText = regexText.Replace(docText, ".  ${a}");
-                }               
-
-                using (StreamWriter sw = new StreamWriter(wordDoc.MainDocumentPart.GetStream(FileMode.Create)))
-                {
-                    sw.Write(docText);
-                }
-            }
-        }
-
-        private static XDocument GetXDocument(WordprocessingDocument myDoc)
-        {
-            // Load the main document part into an XDocument
-            XDocument mainDocumentXDoc;
-            using (Stream str = myDoc.MainDocumentPart.GetStream())
-            using (XmlReader xr = XmlReader.Create(str))
-                mainDocumentXDoc = XDocument.Load(xr);
-            return mainDocumentXDoc;
-        }
-
-        private static void SaveXDocument(WordprocessingDocument myDoc, XDocument mainDocumentXDoc)
-        {
-            // Serialize the XDocument back into the part
-            using (Stream str = myDoc.MainDocumentPart.GetStream(
-                FileMode.Create, FileAccess.Write))
-            using (XmlWriter xw = XmlWriter.Create(str))
-                mainDocumentXDoc.Save(xw);
-        }
-
-        private static void ExitWithError(string message, params object[] args)
-        {
-            var defaultColor = Console.ForegroundColor;
-            Console.ForegroundColor = ConsoleColor.Red;
-            if (args.Any())
-                Console.WriteLine(message, args);
-            else
-                Console.WriteLine(message);
-            Console.ForegroundColor = defaultColor;
             Environment.Exit(1);
+            return 1; // ._.
+        });
+
+        if (inputFiles.Count() < 2) ExitWithError("There must be at least two input files");
+        foreach (var file in inputFiles)
+        {
+            if (!File.Exists(file))
+                ExitWithError("Unable to find {0}", file);
         }
 
+        try {
+            if (verbose) Console.WriteLine("Creating initial document");
+            File.Copy(inputFiles.First(), output, force);
+
+            if (verbose) Console.WriteLine("Opening {0} for writing", output);
+            using (WordprocessingDocument doc = WordprocessingDocument.Open(output, true))
+            {
+                int fileId = 0;
+                foreach (var filepath in inputFiles.Skip(1))
+                {
+                    if (verbose) Console.WriteLine("Adding {0} to {1}", filepath, output);
+                    fileId++;
+                    XNamespace w = "http://schemas.openxmlformats.org/wordprocessingml/2006/main";
+                    XNamespace r = "http://schemas.openxmlformats.org/officeDocument/2006/relationships";
+
+                    string altChuckId = string.Format("AltChuckId{0}", fileId);
+                    var mainPart = doc.MainDocumentPart;
+                    var chunk = mainPart.AddAlternativeFormatImportPart(
+                        "application/vnd.openxmlformats-officedocument.wordprocessingml.document.main+xml",
+                        altChuckId);
+
+                    if (repairSpacing)
+                        RepairSentenceSpacing(filepath);
+
+                    using (FileStream fileStream = File.Open(filepath, FileMode.Open))
+                        chunk.FeedData(fileStream);
+                    var altChunk = new XElement(w + "altChunk", new XAttribute(r + "id", altChuckId));
+                    var mainDocumentXDoc = GetXDocument(doc);
+
+                    mainDocumentXDoc.Root.Element(w + "body").Elements(w + "p").Last().AddAfterSelf(altChunk);
+                    SaveXDocument(doc, mainDocumentXDoc);
+                }
+            }
+
+            if (verbose) Console.WriteLine("Successfully merged all documents");
+        }
+        catch (Exception ex)
+        {
+            if (verbose) ExitWithError(ex.ToString());
+            else ExitWithError("DocxMerge failed to process the files: {0}", ex.Message);
+        }
     }
+
+    private static void RepairSentenceSpacing(string filepath)
+    {
+        using WordprocessingDocument wordDoc = WordprocessingDocument.Open(filepath, true);
+        string docText = null;
+        using (StreamReader sr = new(wordDoc.MainDocumentPart.GetStream()))
+        {
+            docText = sr.ReadToEnd();
+        }
+
+        Regex regexText = RegexText();
+        var refIndex = docText.IndexOf("References");
+        if (refIndex > 0)
+        {
+            var bodyText = docText[..refIndex];
+            var references = docText[bodyText.Length..];
+            docText = regexText.Replace(bodyText, ".  ${a}") + references;
+        }
+        else
+        {
+            docText = regexText.Replace(docText, ".  ${a}");
+        }
+
+        using StreamWriter sw = new(wordDoc.MainDocumentPart.GetStream(FileMode.Create));
+        sw.Write(docText);
+    }
+
+    private static XDocument GetXDocument(WordprocessingDocument myDoc)
+    {
+        // Load the main document part into an XDocument
+        XDocument mainDocumentXDoc;
+        using (Stream str = myDoc.MainDocumentPart.GetStream())
+        using (XmlReader xr = XmlReader.Create(str))
+            mainDocumentXDoc = XDocument.Load(xr);
+        return mainDocumentXDoc;
+    }
+
+    private static void SaveXDocument(WordprocessingDocument myDoc, XDocument mainDocumentXDoc)
+    {
+        // Serialize the XDocument back into the part
+        using Stream str = myDoc.MainDocumentPart.GetStream(
+            FileMode.Create, FileAccess.Write);
+        using XmlWriter xw = XmlWriter.Create(str);
+        mainDocumentXDoc.Save(xw);
+    }
+
+    private static void ExitWithError(string message, params object[] args)
+    {
+        var defaultColor = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Red;
+        if (args.Length != 0)
+            Console.WriteLine(message, args);
+        else
+            Console.WriteLine(message);
+        Console.ForegroundColor = defaultColor;
+        Environment.Exit(1);
+    }
+
+    [GeneratedRegex(@"\. (?<a>[A-Z])")]
+    private static partial Regex RegexText();
 }
